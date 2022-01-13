cc7f74284da40ccd07454893a19748f4cd1f378e
Santosh <santosh.hegde@toptal.com>
2022-01-13 13:04:35 +0100

    test background

--- src/Canvas.js
+++ src/Canvas.js
@@ -8,14 +8,7 @@ import { onUpdateCanvas } from './graphql/subscriptions'
 import { updateCanvas, createCanvas } from './graphql/mutations'
 
 const colors = [
-  '#D50000',
-  '#AA00FF',
-  '#2962FF',
-  '#18FFFF',
-  '#00C853',
-  '#FFD600',
-  '#FF6D00',
-  '#000000'
+  '#D58000'
 ]
 
 function rand() {
